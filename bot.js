4958eaa68ac0ca78d88f17076259df8cb3705890
scollins33 <sean_collins@gmx.com>
2018-01-14 18:07:51 -0500

    modified the WS class to subscribe from full to matches and heartbeat

--- bot.js
+++ bot.js
@@ -6,19 +6,36 @@
 // Their GDAX-only node module
 const GDAX = require('gdax');
 const { PublicClient, WebsocketClient, AuthenticatedClient } = require('gdax');
+const customWS = require('./lib/websocket');
 
 // create PublicClient instance
 pubClient = new PublicClient();
-wsClient = new WebsocketClient(['BTC-USD', 'ETH-USD']);
+// wsClient = new WebsocketClient(['ETH-USD']);
+wsCustom = new customWS(['ETH-USD']);
 
 // get the current order book for ETH-USD
 // level 3 gives all of them, level 1 only gives the ones at the spread. level 2 seems to be a good median
-pubClient
-    .getProductOrderBook('ETH-USD', {level: 2})
-    .then(data => {
-        console.log(data);
-    })
-    .catch(error => {
-        console.log(error);
-    });
 
+// pubClient
+//     .getProductOrderBook('ETH-USD', {level: 2})
+//     .then(data => {
+//         console.log(data);
+//     })
+//     .catch(error => {
+//         console.log(error);
+//     });
+
+let counter = 0;
+
+wsCustom.on('message', data => {
+    if (data.type === 'match') {
+
+        console.log(`----------- ${counter} -----------`);
+        console.log(data);
+
+        counter++;
+    }
+});
+
+wsCustom.on('error', err => { console.log(err) });
+wsCustom.on('close', () => {});

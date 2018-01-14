e5ef7a942015f9a1fb7c7c26ade52174572ec9fd
Sean Collins <sean_collins@gmx.com>
2018-01-14 13:39:37 -0500

    order book works

--- bot.js
+++ bot.js
@@ -9,6 +9,7 @@ const { PublicClient, WebsocketClient, AuthenticatedClient } = require('gdax');
 
 // create PublicClient instance
 pubClient = new PublicClient();
+wsClient = new WebsocketClient(['BTC-USD', 'ETH-USD']);
 
 // get the current order book for ETH-USD
 // level 3 gives all of them, level 1 only gives the ones at the spread. level 2 seems to be a good median
